134608fb260740facbdb7793852a486e14279535
katan4722 <uver7770422@gmail.com>
2021-08-21 20:39:39 +0900

    🔥 console.logの削除

--- src/components/Fetch/GetWeatherFromPlace.jsx
+++ src/components/Fetch/GetWeatherFromPlace.jsx
@@ -4,8 +4,6 @@ import { WeatherData } from "../WeatherData";
 
 // eslint-disable-next-line react/display-name
 export const GetWeatherFromPlace = memo((props) => {
-  console.log(props);
-
   const { data: towndata, error: placeerror } = useSWR(
     props.place
       ? `https://api.openweathermap.org/data/2.5/forecast?q=${props.place}&id=524901&lang=ja&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}`
